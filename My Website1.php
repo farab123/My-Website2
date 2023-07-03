1308c10e91dbf86bec01afe94ec77b5e201b9c9d
farab123 <138360627+farab123@users.noreply.github.com>
2023-07-03 23:07:23 +0500

    files added

---
+++ My Website1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        My Website1
+    </title>
+</head>
+
+<body>
+    <center>
+        <h2 style="color: #d7385e;">Software Testing Mids Slides</h2>
+        <p><b>Click below to download pdf</b></p>
+        <a href="Software Automation Testing Plus Tools_230517_100133.pdf">Download Pdf now</a>
+    </center>
+</body>
+
+</html>
